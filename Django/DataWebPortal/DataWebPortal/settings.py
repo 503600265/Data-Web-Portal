115338536855d6c15cb8fd5cddbba3db08994ae8
Jack Xu <60995631+503600265@users.noreply.github.com>
2022-06-08 15:53:12 -0400

    Update settings.py

--- Django/DataWebPortal/DataWebPortal/settings.py
+++ Django/DataWebPortal/DataWebPortal/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,5 +121,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SITE_ID = 1
